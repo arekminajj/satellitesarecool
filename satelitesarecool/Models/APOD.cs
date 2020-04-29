22b9c69fb589422c119b60e4050c0840f03d0da3
Arek Cios <arek123113@gmail.com>
2020-04-29 16:28:39 +0200

    Crate APOD model

---
+++ satelitesarecool/Models/APOD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace satelitesarecool.Models
+{
+    public partial class Apod
+    {
+        public string Copyright { get; set; }
+        public string Date { get; set; }
+        public string Explanation { get; set; }
+        public string Hdurl { get; set; }
+        public string MediaType { get; set; }
+        public string ServiceVersion { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+}
