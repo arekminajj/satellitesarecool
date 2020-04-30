fa8de7520c27e51ffd4d7a3672a3a6884db2fa73
Arek Cios <arek123113@gmail.com>
2020-04-30 14:57:31 +0200

    update asteroids controller

--- satelitesarecool/Controllers/AsteroidsController.cs
+++ satelitesarecool/Controllers/AsteroidsController.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using satelitesarecool.Models;
 
 namespace satelitesarecool.Controllers
 {
     public class AsteroidsController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            HttpClient client = new HttpClient();
+
+            var day = DateTime.Now.Day;
+            var month = DateTime.Now.Month;
+            var year = DateTime.Now.Year;
+
+            string url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" +
+                         year +
+                         "-" +
+                         month +
+                         "-" +
+                         day +
+                         "&api_key=DEMO_KEY";
+
+            var response = await client.GetStringAsync(url);
+            var asteroids = JsonConvert.DeserializeObject<Asteroid>(response);
+
+            return View(asteroids);
         }
     }
 }
