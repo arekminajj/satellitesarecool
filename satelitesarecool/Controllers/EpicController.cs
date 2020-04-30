c6dd623a7543fda4d0809e1c3cd4a682839466f7
Arek Cios <arek123113@gmail.com>
2020-04-30 16:11:50 +0200

    Add epic controller

---
+++ satelitesarecool/Controllers/EpicController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using satelitesarecool.Models;
+
+namespace satelitesarecool.Controllers
+{
+    public class EpicController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+
+            string url = "https://api.nasa.gov/EPIC/api/natural?api_key=DEMO_KEY";
+            var response = await client.GetStringAsync(url);
+            var epics = JsonConvert.DeserializeObject<Epic>(response);
+
+            return View(epics);
+        }
+    }
+}
