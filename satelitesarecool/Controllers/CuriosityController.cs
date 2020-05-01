4a7b181ac223bf42b4f991f2a7e494df28d5f5b8
Arek Cios <arek123113@gmail.com>
2020-05-01 23:09:43 +0200

    add curiosity controller

---
+++ satelitesarecool/Controllers/CuriosityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using satelitesarecool.Models;
+
+namespace satelitesarecool.Controllers
+{
+    public class CuriosityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return RedirectToAction("Image");
+        }
+
+        public async Task<IActionResult> Image(string solNumber)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY";
+            var response = await client.GetStringAsync(url);
+            var image = JsonConvert.DeserializeObject<List<Curiosity>>(response);
+
+            return View(image.ToList());
+        }
+
+    }
+}
